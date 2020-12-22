0116633f67a4e8a13e2ca4af0eae518279eb17bc
sebas77 <ssense@tiscali.it>
2020-12-22 16:21:31 +0000

    update Example 1 DoofusesMustEat iteration 3

--- Example1-DoofusesMustEat/Iteration3-Jobs/Assets/Code/SortedDoofusesEnginesExecutionGroup.cs
+++ Example1-DoofusesMustEat/Iteration3-Jobs/Assets/Code/SortedDoofusesEnginesExecutionGroup.cs
@@ -1,8 +1,34 @@
+using Svelto.Common;
 using Svelto.DataStructures;
 using Svelto.ECS.Extensions.Unity;
 
 namespace Svelto.ECS.MiniExamples.Example1C
 {
+    public enum DoofusesEngineNames
+    {
+        SpawningDoofusEngine
+      , PlaceFoodOnClickEngine
+      , LookingForFoodDoofusesEngine
+      , VelocityToPositionDoofusesEngine
+      , ConsumingFoodEngine
+    }
+    
+    /// <summary>
+    /// The order of the engines found in the enginesOrder array is the order of execution of engines
+    /// </summary>
+    public struct DoofusesEnginesOrder : ISequenceOrder
+    {
+        public string[] enginesOrder => new[]
+        {
+            nameof(DoofusesEngineNames.SpawningDoofusEngine)
+          , nameof(DoofusesEngineNames.PlaceFoodOnClickEngine)
+          , nameof(DoofusesEngineNames.LookingForFoodDoofusesEngine)
+          , nameof(DoofusesEngineNames.ConsumingFoodEngine)
+          , nameof(DoofusesEngineNames.VelocityToPositionDoofusesEngine)
+          , nameof(JobifiedSveltoEngines.SveltoOverUECS)
+        };
+    }
+    
     public class SortedDoofusesEnginesExecutionGroup : SortedJobifiedEnginesGroup<IJobifiedEngine, DoofusesEnginesOrder>
     {
         public SortedDoofusesEnginesExecutionGroup(FasterList<IJobifiedEngine> engines) : base(engines)
