c17a9bec3d2cf51dccb436512174794520718b27
sebas77 <ssense@tiscali.it>
2020-12-22 18:23:33 +0000

    added comments

--- Example1-DoofusesMustEat/Iteration3-Jobs/Assets/Code/SortedDoofusesEnginesExecutionGroup.cs
+++ Example1-DoofusesMustEat/Iteration3-Jobs/Assets/Code/SortedDoofusesEnginesExecutionGroup.cs
@@ -4,6 +4,8 @@ using Svelto.ECS.Extensions.Unity;
 
 namespace Svelto.ECS.MiniExamples.Example1C
 {
+    ///it's important to note that the names of the engines used in the ISequenceOrder, do NOT need to come from the
+    /// same enum. This will allow the user to declare enums in their own assemblies. 
     public enum DoofusesEngineNames
     {
         SpawningDoofusEngine
@@ -28,7 +30,9 @@ namespace Svelto.ECS.MiniExamples.Example1C
           , nameof(JobifiedSveltoEngines.SveltoOverUECS)
         };
     }
-    
+    //The engines added in this group will need to be marked as Sequenced and signed with the enums used inside
+    //the ISequenceOrder struct. If the ISequenceOrder names and the signatures do not match, Svelto will throw an
+    //exception. If they match, the engines will be executed using the ISequencedOrder declaration.
     public class SortedDoofusesEnginesExecutionGroup : SortedJobifiedEnginesGroup<IJobifiedEngine, DoofusesEnginesOrder>
     {
         public SortedDoofusesEnginesExecutionGroup(FasterList<IJobifiedEngine> engines) : base(engines)
