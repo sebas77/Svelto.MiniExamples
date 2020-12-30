adcf47d564a68fd1c72d42208ef370df278e18b0
sebas77 <ssense@tiscali.it>
2020-12-30 11:20:16 +0000

    Example 6 WIP: fix asmdef and improve code

--- Example6-Unity Hybrid-OOP Abstraction/Assets/Code/WithOOPLayer/Engines/SelectNewParentEngine.cs
+++ Example6-Unity Hybrid-OOP Abstraction/Assets/Code/WithOOPLayer/Engines/SelectNewParentEngine.cs
@@ -1,3 +1,4 @@
+using Svelto.DataStructures;
 using Svelto.ECS.Example.OOPAbstraction.OOPLayer;
 using UnityEngine;
 
@@ -11,18 +12,45 @@ namespace Svelto.ECS.Example.OOPAbstraction.WithOOPLayer
         {
             if (Input.anyKeyDown)
             {
-                uint sphereIndex = 0;
-                foreach (var ((bufferSphere, egids, sphereCount), _) in entitiesDB.QueryEntities<ObjectParentComponent, EGIDComponent>(ExampleGroups.SpherePrimitive.Groups))
-                foreach (var ((bufferCube, cubeCount), _) in entitiesDB.QueryEntities<ObjectIndexComponent>(ExampleGroups.CubePrimitive.Groups))
+                var groupsEnumerable = entitiesDB
+                                      .QueryEntities<ObjectIndexComponent>(ExampleGroups.CubePrimitive.Groups)
+                                      .GetEnumerator();
+
+                uint                     sphereIndex = 0;
+                NB<ObjectIndexComponent> bufferCube  = default;
+                int                      cubeIndex   = 0, cubeCount = 0;
+
+                //as long as there are valid groups SpherePrimitive 
+                foreach (var ((bufferSphere, egids, sphereCount), _) in entitiesDB
+                   .QueryEntities<ObjectParentComponent, EGIDComponent>(ExampleGroups.SpherePrimitive.Groups))
                 {
-                    while (sphereIndex < sphereCount && sphereIndex < cubeCount)
+                    //as long as in the current group there are still spheres to iterate
+                    while (sphereIndex < sphereCount)
                     {
-                        bufferSphere[sphereIndex].parent = bufferCube[(uint) ((_index + sphereIndex) % cubeCount)].index;
+                        //if there aren't more cubes to iterate
+                        if (cubeIndex == cubeCount)
+                        {
+                            //move to the next cube group, if any, otherwise return
+                            if (groupsEnumerable.MoveNext() == false)
+                                goto end;
+
+                            ((bufferCube, cubeCount), _) = groupsEnumerable.Current;
+                            cubeIndex                    = 0;
+                        }
+
+                        //the current sphere is parented with the current cube
+                        bufferSphere[sphereIndex].parent = bufferCube[(uint) ((_index + cubeIndex) % cubeCount)].index;
+
+                        //publish the change
                         entitiesDB.PublishEntityChange<ObjectParentComponent>(egids[sphereIndex].ID);
+
+                        //move to the next sphere and next cube
                         sphereIndex++;
+                        cubeIndex++;
                     }
                 }
-
+end:
+                //move cursor so that different parents are chosen every time
                 _index++;
             }
         }
