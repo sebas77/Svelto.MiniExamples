d760c31bd60abe5c7d37441ecbf0faaa6e6f541b
sebas77 <ssense@tiscali.it>
2022-04-12 13:29:13 +0100

    update hello world demo

--- Example5-Net-HelloWorld/Svelto.ECS.Example/src/Svelto-ECS-Simplest-Example-Ever/Example/SimpleContext.cs
+++ Example5-Net-HelloWorld/Svelto.ECS.Example/src/Svelto-ECS-Simplest-Example-Ever/Example/SimpleContext.cs
@@ -15,25 +15,25 @@ public static class ExclusiveGroups
 namespace Svelto.ECS.Vanilla.Example
 {
     /// <summary>
-    ///     The Context is the framework starting point.
-    ///     As Composition root, it gives to the coder the responsibility to create, initialize and inject dependencies.
+    ///     The Context is the application starting point.
+    ///     As a Composition root, it gives to the coder the responsibility to create, initialize and
+    ///     inject dependencies.
     ///     Every application can have more than one context and every context can have one
     ///     or more composition roots (a facade, but even a factory, can be a composition root)
     /// </summary>
     public class SimpleContext
     {
-        readonly EnginesRoot _enginesRoot;
-
         public SimpleContext()
         {
-            //An EnginesRoot holds all the engines created. it needs a EntitySubmissionScheduler to know when to
-            //add the EntityViews generated inside the EntityDB.
             var simpleSubmissionEntityViewScheduler = new SimpleEntitiesSubmissionScheduler();
+            //An EnginesRoot holds all the engines and entities created. it needs a EntitySubmissionScheduler to know when to
+            //add previously built entities to the Svelto database. Using the SimpleEntitiesSubmissionScheduler
+            //is expected as it gives complete control to the user about when the submission happens
             _enginesRoot = new EnginesRoot(simpleSubmissionEntityViewScheduler);
 
-            //an EnginesRoot must never be injected inside other classes only IEntityFactory and IEntityFunctions
-            //implementation can
-            var entityFactory   = _enginesRoot.GenerateEntityFactory();
+            //an entity factory allows to build entities inside engines
+            var entityFactory = _enginesRoot.GenerateEntityFactory();
+            //the entity functions allows other operations on entities, like remove and swap
             var entityFunctions = _enginesRoot.GenerateEntityFunctions();
 
             //Add the Engine to manage the SimpleEntities
@@ -43,10 +43,11 @@ namespace Svelto.ECS.Vanilla.Example
             //build Entity with ID 0 in group0
             entityFactory.BuildEntity<SimpleEntityDescriptor>(new EGID(0, ExclusiveGroups.group0));
 
-            //as we are using a basic scheduler, we need to schedule the entity submission ourselves
+            //submit the previously built entities to the Svelto database
             simpleSubmissionEntityViewScheduler.SubmitEntities();
 
-            //as we don't have any ticking system for this basic example, we tick explicitly 
+            //as Svelto doesn't provide an engine ticking system, it's the user's responsibility to
+            //update engines  
             behaviourForEntityClassEngine.Update();
 
             Console.Log("Done - click any button to quit");
@@ -55,31 +56,31 @@ namespace Svelto.ECS.Vanilla.Example
 
             Environment.Exit(0);
         }
-   }
-    
-        //An EntityComponent must always implement the IEntityComponent interface
+
+        readonly EnginesRoot _enginesRoot;
+    }
+
+    //An EntityComponent must always implement the IEntityComponent interface
     //don't worry, boxing/unboxing will never happen.
     public struct EntityComponent : IEntityComponent
     {
-        public int  counter;
+        public int counter;
     }
 
     /// <summary>
     ///     The EntityDescriptor identifies your Entity. It's essential to identify
     ///     your entities with a name that comes from the Game Design domain.
     /// </summary>
-    class SimpleEntityDescriptor : GenericEntityDescriptor<EntityComponent>
-    {}
+    class SimpleEntityDescriptor : GenericEntityDescriptor<EntityComponent> { }
 
     namespace SimpleEntityEngine
     {
         public class BehaviourForEntityClassEngine :
-                //this interface makes the engine reactive, it's absolutely optional, you need to read my articles
-                //and wiki about it.
-                IReactOnAddAndRemove<EntityComponent>,
-                IReactOnSwap<EntityComponent>,
-                //while this interface is optional too, it's almost always used as it gives access to the entitiesDB
-                IQueryingEntitiesEngine
+            //this interface makes the engine reactive, it's absolutely optional, you need to read my articles
+            //and wiki about it.
+            IReactOnAddEx<EntityComponent>, IReactOnSwapEx<EntityComponent>,
+            //while this interface is optional too, it's almost always used as it gives access to the entitiesDB
+            IQueryingEntitiesEngine
         {
             //extra entity functions
             readonly IEntityFunctions _entityFunctions;
@@ -93,26 +94,14 @@ namespace Svelto.ECS.Vanilla.Example
 
             public void Ready() { }
 
-            public void Add(ref EntityComponent entityView, EGID egid)
-            {
-                _entityFunctions.SwapEntityGroup<SimpleEntityDescriptor>(egid, ExclusiveGroups.group1);
-            }
-
-            public void Remove(ref EntityComponent entityView, EGID egid) { }
-            
-            public void MovedTo(ref EntityComponent entityComponent, ExclusiveGroupStruct previousGroup, EGID egid)
-            {
-                Console.Log("Swap happened");
-            }
-
             public void Update()
             {
-                var (entityViews, count) = entitiesDB.QueryEntities<EntityComponent>(ExclusiveGroups.group1);
+                var (components, count) = entitiesDB.QueryEntities<EntityComponent>(ExclusiveGroups.group1);
 
                 if (count > 0)
                 {
                     for (var i = 0; i < count; i++)
-                        entityViews[i].counter++;
+                        components[i].counter++;
 
                     Console.Log("Entity Struct engine executed");
                 }
@@ -121,7 +110,25 @@ namespace Svelto.ECS.Vanilla.Example
                     throw new Exception("can't be");
                 }
             }
+
+            public void Add
+            ((uint start, uint end) rangeOfEntities, in EntityCollection<EntityComponent> collection
+           , ExclusiveGroupStruct groupID)
+            {
+                var (_, entityIDs, _) = collection;
+
+                for (uint index = rangeOfEntities.start; index < rangeOfEntities.end; index++)
+                    _entityFunctions.SwapEntityGroup<SimpleEntityDescriptor>(
+                        new EGID(entityIDs[index], groupID), ExclusiveGroups.group1);
+            }
+
+            public void MovedTo
+            ((uint start, uint end) rangeOfEntities, in EntityCollection<EntityComponent> collection
+           , ExclusiveGroupStruct fromGroup, ExclusiveGroupStruct toGroup)
+            {
+                for (uint index = rangeOfEntities.start; index < rangeOfEntities.end; index++)
+                    Console.Log("Swap happened");
+            }
         }
     }
-
 }
